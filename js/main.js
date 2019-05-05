999d0c80846c7659cd1ab105bd5d5879c6ffdb66
Mayur Dudhani <mdudhani@gmail.com>
2019-05-05 16:17:05 -0700

    UI: Correct paths. Users ve header n footer.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function(){
-  $("header").load("common/header.html"); 
-  $("footer").load("common/footer.html"); 
-  $("#events").load("common/events.html"); 
+  $("header").load("/common/header.html", function( response, status, xhr ) {
+    if (status === "error") {
+      $("header").load("../common/header.html");
+    }
+  }); 
+  $("footer").load("/common/footer.html", function( response, status, xhr ) {
+    if (status === "error") {
+      $("footer").load("../common/footer.html");
+    }
+  }); 
+  $("#events").load("/common/events.html"); 
 });
